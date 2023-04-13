ec4a0912645d305197a2667a32ffc35debd0f2f5
Oleomoreira <lmoreira908@gmail.com>
2023-04-13 18:24:59 -0300

    primeiro commit java logica

---
+++ java_tokio2023/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,19 @@
+package logica.aulas;
+
+import java.util.Scanner;
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome");
+		String nome = entrada.nextLine();
+		
+		System.out.println("Digite sua idade");
+		System.out.println("sua idade é:" + entrada.nextLine());
+		
+
+	}
+
+}
